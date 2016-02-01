a7b1df4ed86f0106162209e054a084e4dd0e3186
Jonathan Minton <nate.minton@gmail.com>
2016-02-01 12:03:28 +0000

    create 8 fold classification of deaths based on icd 9 and icd 10 codes (to do: icd 8)

--- script.R
+++ script.R
@@ -247,6 +247,591 @@ icd_09_3cause <- icd_09_3cause %>% filter(race != "" & sex != "" & year != "")
 rm(tmp1, tmp2)
 
 
+icd_08_3cause <- icd_08_3cause  %>% 
+  mutate(icd_class = "icd_08")  %>% 
+  select(icd_class, year, age, race, sex, cause, death_count, population_count) %>% 
+  arrange(year, age, race, sex, cause)
+
+
+icd_09_3cause <- icd_09_3cause %>% 
+  mutate(icd_class = "icd_09") %>% 
+  select(icd_class, year, age, race, sex, cause, death_count, population_count) %>% 
+  arrange(year, age, race, sex, cause)
+
+
+icd_08_09_3cause <- bind_rows(icd_08_3cause, icd_09_3cause)
+
+write_csv(icd_08_09_3cause, path = "data/tidied/icd_08_09_3cause.csv")
+
+
+
+
+# 8 causes , icd 8 , icd 9 and icd 10 -------------------------------------
+
+# The 8 causes are 
+# all cause 
+# all external 
+# asssault/homicide
+# self harm (intentional or undetermined)
+# intentional self harm
+# undetermined self harm
+# non-external
+# vehicle-based
+
+
+
+
+# 8 cause, icd 8 ----------------------------------------------------------
+
+
+
+
+# 8 causes - icd 9  -------------------------------------------------------
+
+
+icd_09_all_cause <- read_delim(
+  "data/icd_09_year_age_gender_race_allcause.txt",
+  delim = "\t",
+  col_types = paste(rep("c", 12), collapse = "")
+)
+
+icd_09_all_cause <- icd_09_all_cause %>% 
+  filter(Notes == "") %>% 
+  select(
+    year = `Year Code`, age = `Age Group Code`, sex = Gender, race = Race, 
+    death_count = Deaths, population_count = Population
+         ) %>% 
+  mutate(
+    sex = tolower(sex),
+    r2 = recode(race, 
+                recodes = "
+                'Black or African American' = 'black';
+                'Other Race' = 'other';
+                'White' = 'white'
+                "
+                )
+    ) %>% 
+  mutate(cause = "all_cause") %>% 
+  mutate(icd_class = "icd_09") %>% 
+  select(icd_class, year, age, sex, race = r2, cause, death_count, population_count) %>% 
+  filter(population_count != "Not Applicable") %>% 
+  mutate(
+    death_count = as.numeric(death_count), 
+    population_count = as.numeric(population_count)
+         ) 
+
+
+# external 
+
+icd_09_external <- read_delim(
+  "data/icd_09_year_age_gender_race_external.txt",
+  delim = "\t",
+  col_types = paste(rep("c", 12), collapse = "")
+)
+
+icd_09_external <- icd_09_external %>% 
+  filter(Notes == "") %>% 
+  select(
+    year = `Year Code`, age = `Age Group Code`, sex = Gender, race = Race, 
+    death_count = Deaths, population_count = Population
+  ) %>% 
+  mutate(
+    sex = tolower(sex),
+    r2 = recode(race, 
+                recodes = "
+                'Black or African American' = 'black';
+                'Other Race' = 'other';
+                'White' = 'white'
+                "
+    )
+  ) %>% 
+  mutate(cause = "all_external") %>% 
+  mutate(icd_class = "icd_09") %>% 
+  select(icd_class, year, age, sex, race = r2, cause, death_count, population_count) %>% 
+  filter(population_count != "Not Applicable") %>% 
+  mutate(
+    death_count = as.numeric(death_count), 
+    population_count = as.numeric(population_count)
+  )  
+
+
+# the other categories can all be extracted from the following file 
+
+icd_09_icdsub <- read_delim(
+  "data/icd_09_year_age_gender_race_icd_subchapter.txt",
+  delim = "\t",
+  col_types = paste(rep("c", 14), collapse = "")
+)
+
+# Motor related 
+icd_09_motor <- icd_09_icdsub %>% 
+  filter(`ICD Sub-Chapter Code` %in% c(
+    "E810-E819", "E820-E825", "E826-E829")) %>% 
+  filter(Notes == "") %>% 
+  select(year = Year, age = `Age Group Code`, sex = Gender, race = Race, 
+         death_count = Deaths, population_count = Population) %>% 
+  filter(population_count != "Not Applicable") %>% 
+  mutate(
+    death_count = as.numeric(death_count), 
+    population_count = as.numeric(population_count)
+    ) %>% 
+  group_by(year, age, sex, race) %>% 
+  summarise(
+    death_count = sum(death_count), 
+    population_count = sum(population_count)
+    ) %>% 
+  ungroup %>% 
+  mutate(
+    sex = tolower(sex),
+    r2 = recode(race, 
+                recodes = "
+                'Black or African American' = 'black';
+                'Other Race' = 'other';
+                'White' = 'white'
+                "
+    )
+    ) %>% 
+  mutate(cause = "motor") %>% 
+  mutate(icd_class = "icd_09") %>% 
+  select(icd_class, year, age, sex, race = r2, cause, death_count, population_count) 
+
+
+  
+# Assault and homicide
+icd_09_assault <- icd_09_icdsub %>% 
+  filter(`ICD Sub-Chapter Code` %in% c("E960-E969")) %>% 
+  filter(Notes == "") %>% 
+  select(year = Year, age = `Age Group Code`, sex = Gender, race = Race, 
+         death_count = Deaths, population_count = Population) %>% 
+  filter(population_count != "Not Applicable") %>% 
+  mutate(
+    death_count = as.numeric(death_count), 
+    population_count = as.numeric(population_count)
+  ) %>% 
+  mutate(
+    sex = tolower(sex),
+    r2 = recode(race, 
+                recodes = "
+                'Black or African American' = 'black';
+                'Other Race' = 'other';
+                'White' = 'white'
+                "
+    )
+    ) %>% 
+  mutate(cause = "assault") %>% 
+  mutate(icd_class = "icd_09") %>% 
+  select(icd_class, year, age, sex, race = r2, cause, death_count, population_count) 
+
+# intentional self harm
+icd_09_intent <- icd_09_icdsub %>% 
+  filter(`ICD Sub-Chapter Code` %in% c("E950-E959")) %>% 
+  filter(Notes == "") %>% 
+  select(year = Year, age = `Age Group Code`, sex = Gender, race = Race, 
+         death_count = Deaths, population_count = Population) %>% 
+  filter(population_count != "Not Applicable") %>% 
+  mutate(
+    death_count = as.numeric(death_count), 
+    population_count = as.numeric(population_count)
+  ) %>% 
+  mutate(
+    sex = tolower(sex),
+    r2 = recode(race, 
+                recodes = "
+                'Black or African American' = 'black';
+                'Other Race' = 'other';
+                'White' = 'white'
+                "
+    )
+    ) %>% 
+  mutate(cause = "intentional_self_harm") %>% 
+  mutate(icd_class = "icd_09") %>% 
+  select(icd_class, year, age, sex, race = r2, cause, death_count, population_count) 
+
+# undetermined intent
+icd_09_undetermined <- icd_09_icdsub %>% 
+  filter(`ICD Sub-Chapter Code` %in% c("E980-E989")) %>% 
+  filter(Notes == "") %>% 
+  select(year = Year, age = `Age Group Code`, sex = Gender, race = Race, 
+         death_count = Deaths, population_count = Population) %>% 
+  filter(population_count != "Not Applicable") %>% 
+  mutate(
+    death_count = as.numeric(death_count), 
+    population_count = as.numeric(population_count)
+  ) %>% 
+  mutate(
+    sex = tolower(sex),
+    r2 = recode(race, 
+                recodes = "
+                'Black or African American' = 'black';
+                'Other Race' = 'other';
+                'White' = 'white'
+                "
+    )
+    ) %>% 
+  mutate(cause = "undetermined") %>% 
+  mutate(icd_class = "icd_09") %>% 
+  select(icd_class, year, age, sex, race = r2, cause, death_count, population_count) 
+
+
+
+
+# to dos 
+
+# 1 ) calculate non external 
+# 2) combine intentioanl self harm and undetermined into a single category
+# 3) bind all together 
+
+
+
+# 1 ) calculate non external 
+
+tmp1 <- icd_09_all_cause %>% 
+  select(year, age, race, sex, cause, death_count)
+
+tmp2 <- icd_09_external %>% 
+  select(year, age, race, sex, cause, death_count)
+
+tmp3 <- bind_rows(tmp1, tmp2)
+rm(tmp1, tmp2)
+
+
+tmp4 <- tmp3 %>% 
+  spread(key = cause, value = death_count) %>% 
+  mutate(non_external = all_cause - all_external) %>%
+  gather(key = "cause", value = "death_count", -year, -age, -race, -sex) %>% 
+  filter(cause == "non_external")
+
+rm(tmp3)
+
+tmp5 <- icd_09_all_cause %>% 
+  select(year, age, race, sex, population_count) %>% 
+  ungroup
+
+icd_09_nonexternal <- tmp4 %>% inner_join(tmp5)
+
+rm(tmp4, tmp5)
+
+icd_09_nonexternal <- icd_09_nonexternal %>% 
+  mutate(icd_class = "icd_09") %>% 
+  select(icd_class, year, age, race, sex, cause, death_count, population_count)
+
+# 2) combine intentional self harm and undetermined into a single category
+
+# bind intent and undetermined together
+
+tmp1 <- icd_09_undetermined %>% bind_rows(icd_09_intent)
+
+tmp2 <- tmp1 %>% 
+  select(-population_count) %>% 
+  spread(key = cause, value = death_count) %>% 
+  mutate(intentional_or_undetermined_self_harm = intentional_self_harm + undetermined) %>% 
+  gather(key = "cause", value = "death_count", -icd_class, -year, -age, -race, -sex) %>% 
+  filter(cause == "intentional_or_undetermined_self_harm")
+
+rm(tmp1)
+
+tmp3 <- icd_09_undetermined %>% 
+  select(icd_class, year, age, race, sex, population_count)
+
+icd_09_self_harm <- tmp2 %>% inner_join(tmp3)
+
+rm(tmp2, tmp3)
+
+# 3) bind all together 
+
+icd_09_8cause <- icd_09_intent %>% 
+  bind_rows(icd_09_assault) %>% 
+  bind_rows(icd_09_all_cause) %>% 
+  bind_rows(icd_09_motor) %>% 
+  bind_rows(icd_09_external) %>% 
+  bind_rows(icd_09_nonexternal) %>% 
+  bind_rows(icd_09_self_harm) %>% 
+  bind_rows(icd_09_undetermined)
+
+# 4 ) write to file
+
+write_csv(icd_09_8cause, path = "data/tidied/icd_09_8cause.csv")
+
+
+
+
+# 8 causes, icd 10 --------------------------------------------------------
+
+
+# Now to look at what we can do with icd_10
+
+icd_10_external <- read_delim(
+  "data/icd_10_underlying_single_age_year_age_gender_race_hispanic_external.txt", 
+  delim = "\t", col_types = paste(rep("c", 14), collapse = "")
+  )
+
+icd_10_external <- icd_10_external %>% 
+    filter(Notes != "Total") %>% 
+    select(-Notes) %>% 
+    select(year = `Year Code`, age = `Single-Year Ages Code`, 
+           sex = Gender, race =Race, hispanic = `Hispanic Origin`, 
+           death_count = Deaths, population_count = Population
+           ) %>% 
+    filter(population_count != "Not Applicable") %>% 
+    mutate(
+      group = ifelse(race == "White" & hispanic == "Not Hispanic or Latino", "white", "other"),
+      group = ifelse(race == "Black or African American" & hispanic == "Not Hispanic or Latino", "black", group), 
+      group = ifelse(hispanic =="Hispanic or Latino", "hispanic", group)
+    ) %>% 
+  mutate(sex = tolower(sex)) %>% 
+  mutate(icd_class = "icd_10") %>% 
+  mutate(cause = "all_external") %>% 
+  select(icd_class, year, age, race = group, sex, cause, death_count, population_count) %>% 
+  mutate(
+    death_count = as.numeric(str_replace_all(death_count, ",", "")),
+    population_count = as.numeric(str_replace_all(population_count, ",", ""))
+  ) %>% 
+  group_by(icd_class, year, age, race, sex, cause) %>% 
+  summarise(death_count = sum(death_count), population_count = sum(population_count)) 
+
+
+
+# ICD 10 - all cause 
+
+icd_10_allcause <- read_delim(
+  "data/icd_10_underlying_single_age_year_gender_hisp_race_allcause.txt", 
+  delim = "\t", col_types = paste(rep("c", 14), collapse = "")
+)
+
+icd_10_allcause <- icd_10_allcause %>% 
+  filter(Notes != "Total") %>% 
+  select(-Notes) %>% 
+  select(year = `Year Code`, age = `Single-Year Ages Code`, 
+         sex = Gender, race =Race, hispanic = `Hispanic Origin`, 
+         death_count = Deaths, population_count = Population
+  ) %>% 
+  filter(population_count != "Not Applicable") %>% 
+  mutate(
+    group = ifelse(race == "White" & hispanic == "Not Hispanic or Latino", "white", "other"),
+    group = ifelse(race == "Black or African American" & hispanic == "Not Hispanic or Latino", "black", group), 
+    group = ifelse(hispanic =="Hispanic or Latino", "hispanic", group)
+  ) %>% 
+  mutate(sex = tolower(sex)) %>% 
+  mutate(icd_class = "icd_10") %>% 
+  mutate(cause = "all_cause") %>% 
+  select(icd_class, year, age, race = group, sex, cause, death_count, population_count) %>% 
+  mutate(
+    death_count = as.numeric(str_replace_all(death_count, ",", "")),
+    population_count = as.numeric(str_replace_all(population_count, ",", ""))
+  ) %>% 
+  group_by(icd_class, year, age, race, sex, cause) %>% 
+  summarise(death_count = sum(death_count), population_count = sum(population_count))
+
+
+
+
+# ICD 10 - vehicle
+
+icd_10_vehicle <- read_delim(
+  "data/icd_10_underlying_singleage_year_gender_hisp_race_motor.txt", 
+  delim = "\t", col_types = paste(rep("c", 14), collapse = "")
+)
+
+icd_10_vehicle <- icd_10_vehicle %>% 
+  filter(Notes != "Total") %>% 
+  select(-Notes) %>% 
+  select(year = `Year Code`, age = `Single-Year Ages Code`, 
+         sex = Gender, race =Race, hispanic = `Hispanic Origin`, 
+         death_count = Deaths, population_count = Population
+  ) %>% 
+  filter(population_count != "Not Applicable") %>% 
+  mutate(
+    group = ifelse(race == "White" & hispanic == "Not Hispanic or Latino", "white", "other"),
+    group = ifelse(race == "Black or African American" & hispanic == "Not Hispanic or Latino", "black", group), 
+    group = ifelse(hispanic =="Hispanic or Latino", "hispanic", group)
+  ) %>% 
+  mutate(sex = tolower(sex)) %>% 
+  mutate(icd_class = "icd_10") %>% 
+  mutate(cause = "vehicle") %>% 
+  select(icd_class, year, age, race = group, sex, cause, death_count, population_count) %>% 
+  mutate(
+    death_count = as.numeric(str_replace_all(death_count, ",", "")),
+    population_count = as.numeric(str_replace_all(population_count, ",", ""))
+  ) %>% 
+  group_by(icd_class, year, age, race, sex, cause) %>% 
+  summarise(death_count = sum(death_count), population_count = sum(population_count))
+
+
+
+
+# ICD 10 - assault
+
+icd_10_assault <- read_delim(
+  "data/icd_10_underlying_single_age_year_race_hispanic_gender_assault.txt", 
+  delim = "\t", col_types = paste(rep("c", 14), collapse = "")
+)
+
+icd_10_assault <- icd_10_assault %>% 
+  filter(Notes != "Total") %>% 
+  select(-Notes) %>% 
+  select(year = `Year Code`, age = `Single-Year Ages Code`, 
+         sex = Gender, race =Race, hispanic = `Hispanic Origin`, 
+         death_count = Deaths, population_count = Population
+  ) %>% 
+  filter(population_count != "Not Applicable") %>% 
+  mutate(
+    group = ifelse(race == "White" & hispanic == "Not Hispanic or Latino", "white", "other"),
+    group = ifelse(race == "Black or African American" & hispanic == "Not Hispanic or Latino", "black", group), 
+    group = ifelse(hispanic =="Hispanic or Latino", "hispanic", group)
+  ) %>% 
+  mutate(sex = tolower(sex)) %>% 
+  mutate(icd_class = "icd_10") %>% 
+  mutate(cause = "assault") %>% 
+  select(icd_class, year, age, race = group, sex, cause, death_count, population_count) %>% 
+  mutate(
+    death_count = as.numeric(str_replace_all(death_count, ",", "")),
+    population_count = as.numeric(str_replace_all(population_count, ",", ""))
+  ) %>% 
+  group_by(icd_class, year, age, race, sex, cause) %>% 
+  summarise(death_count = sum(death_count), population_count = sum(population_count))
+
+
+
+# ICD 10 - intentional self harm
+
+icd_10_intent <- read_delim(
+  "data/icd_10_underlying_singleage_year_gender_hisp_race_intent.txt", 
+  delim = "\t", col_types = paste(rep("c", 14), collapse = "")
+)
+
+icd_10_intent <- icd_10_intent %>% 
+  filter(Notes != "Total") %>% 
+  select(-Notes) %>% 
+  select(year = `Year Code`, age = `Single-Year Ages Code`, 
+         sex = Gender, race =Race, hispanic = `Hispanic Origin`, 
+         death_count = Deaths, population_count = Population
+  ) %>% 
+  filter(population_count != "Not Applicable") %>% 
+  mutate(
+    group = ifelse(race == "White" & hispanic == "Not Hispanic or Latino", "white", "other"),
+    group = ifelse(race == "Black or African American" & hispanic == "Not Hispanic or Latino", "black", group), 
+    group = ifelse(hispanic =="Hispanic or Latino", "hispanic", group)
+  ) %>% 
+  mutate(sex = tolower(sex)) %>% 
+  mutate(icd_class = "icd_10") %>% 
+  mutate(cause = "intentional_self_harm") %>% 
+  select(icd_class, year, age, race = group, sex, cause, death_count, population_count) %>% 
+  mutate(
+    death_count = as.numeric(str_replace_all(death_count, ",", "")),
+    population_count = as.numeric(str_replace_all(population_count, ",", ""))
+  ) %>% 
+  group_by(icd_class, year, age, race, sex, cause) %>% 
+  summarise(death_count = sum(death_count), population_count = sum(population_count))
+
+
+
+# ICD 10, Undetermined intent
+icd_10_undetermined <- read_delim(
+  "data/icd_10_underlying_year_singleage_gender_hisp_race_undetermined.txt", 
+  delim = "\t", col_types = paste(rep("c", 14), collapse = "")
+)
+
+icd_10_undetermined <- icd_10_undetermined %>% 
+  filter(Notes != "Total") %>% 
+  select(-Notes) %>% 
+  select(year = `Year Code`, age = `Single-Year Ages Code`, 
+         sex = Gender, race =Race, hispanic = `Hispanic Origin`, 
+         death_count = Deaths, population_count = Population
+  ) %>% 
+  filter(population_count != "Not Applicable") %>% 
+  mutate(
+    group = ifelse(race == "White" & hispanic == "Not Hispanic or Latino", "white", "other"),
+    group = ifelse(race == "Black or African American" & hispanic == "Not Hispanic or Latino", "black", group), 
+    group = ifelse(hispanic =="Hispanic or Latino", "hispanic", group)
+  ) %>% 
+  mutate(sex = tolower(sex)) %>% 
+  mutate(icd_class = "icd_10") %>% 
+  mutate(cause = "undetermined") %>% 
+  select(icd_class, year, age, race = group, sex, cause, death_count, population_count) %>% 
+  mutate(
+    death_count = as.numeric(str_replace_all(death_count, ",", "")),
+    population_count = as.numeric(str_replace_all(population_count, ",", ""))
+  ) %>% 
+  group_by(icd_class, year, age, race, sex, cause) %>% 
+  summarise(death_count = sum(death_count), population_count = sum(population_count))
+
+
+
+# to dos 
+
+# 1 ) calculate non external 
+# 2) combine intentioanl self harm and undetermined into a single category
+# 3) bind all together 
+
+
+
+# 1 ) calculate non external 
+
+tmp1 <- icd_10_allcause %>% 
+  select(year, age, race, sex, cause, death_count)
+
+tmp2 <- icd_10_external %>% 
+  select(year, age, race, sex, cause, death_count)
+
+tmp3 <- bind_rows(tmp1, tmp2)
+rm(tmp1, tmp2)
+
+
+tmp4 <- tmp3 %>% 
+  spread(key = cause, value = death_count) %>% 
+  mutate(non_external = all_cause - all_external) %>%
+  gather(key = "cause", value = "death_count", -icd_class, -year, -age, -race, -sex) %>% 
+  filter(cause == "non_external")
+
+rm(tmp3)
+
+tmp5 <- icd_10_allcause %>% 
+  select(year, age, race, sex, population_count) %>% 
+  ungroup
+
+icd_10_nonexternal <- tmp4 %>% inner_join(tmp5)
+
+rm(tmp4, tmp5)
+# 2) combine intentional self harm and undetermined into a single category
+
+# bind intent and undetermined together
+
+tmp1 <- icd_10_undetermined %>% bind_rows(icd_10_intent)
+
+tmp2 <- tmp1 %>% 
+  select(-population_count) %>% 
+  spread(key = cause, value = death_count) %>% 
+  mutate(intentional_or_undetermined_self_harm = intentional_self_harm + undetermined) %>% 
+  gather(key = "cause", value = "death_count", -icd_class, -year, -age, -race, -sex) %>% 
+  filter(cause == "intentional_or_undetermined_self_harm")
+
+rm(tmp1)
+
+tmp3 <- icd_10_undetermined %>% 
+  select(icd_class, year, age, race, sex, population_count)
+
+icd_10_self_harm <- tmp2 %>% inner_join(tmp3)
+
+rm(tmp2, tmp3)
+
+# 3) bind all together 
+
+icd_10_8cause <- icd_10_intent %>% 
+  bind_rows(icd_10_assault) %>% 
+  bind_rows(icd_10_allcause) %>% 
+  bind_rows(icd_10_vehicle) %>% 
+  bind_rows(icd_10_external) %>% 
+  bind_rows(icd_10_nonexternal) %>% 
+  bind_rows(icd_10_self_harm) %>% 
+  bind_rows(icd_10_undetermined)
+
+# 4 ) write to file
+
+write_csv(icd_10_8cause, path = "data/tidied/icd_10_8cause.csv")
+
+
+
+
 # Progress : 
 #   
 #   Found and extracted data on legal intervention from 1968-1974
