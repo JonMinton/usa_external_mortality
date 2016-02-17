46a731c7a7f387ebb64e8052769abfdc6d4ecac6
Jonathan Minton <nate.minton@gmail.com>
2016-02-17 09:06:12 +0000

    create stata version of main tidied data

---
+++ scripts/resave_csv_as_dta.R
@@ -0,0 +1,17 @@
+# Script for converting tidied doc to dta format
+
+rm(list = ls())
+
+require(readr)
+require(foreign)
+
+require(tidyr)
+require(stringr)
+require(dplyr)
+
+
+dta <- read_csv("data/tidied/icd_8_to_10_8fold.csv")
+
+
+write.dta(dta, "data/tidied/icd_8_to_10_8fold.dta")
+
